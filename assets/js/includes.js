0f1783052f1cf2fa29b3a5470519325a34420c8d
venkatpalika-oss <venkat.palika@gmail.com>
2026-02-08 14:58:55 +0400

    Update includes.js

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -1,12 +1,18 @@
 function loadInclude(id, url) {
   fetch(url)
-    .then(res => res.text())
+    .then(response => {
+      if (!response.ok) throw new Error("Include not found");
+      return response.text();
+    })
     .then(html => {
       document.getElementById(id).innerHTML = html;
+    })
+    .catch(() => {
+      // Fail silently (prevents GitHub 404 page injection)
     });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  loadInclude("site-header", "includes/header.html");
-  loadInclude("site-footer", "includes/footer.html");
+  loadInclude("site-header", "../includes/header.html");
+  loadInclude("site-footer", "../includes/footer.html");
 });
