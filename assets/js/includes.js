ec16bf2cbf16198c4d6f1f30ef44570027d8e11c
venkatpalika-oss <venkat.palika@gmail.com>
2026-02-08 14:39:07 +0400

    Create includes.js

---
+++ assets/js/includes.js
@@ -0,0 +1,12 @@
+function loadInclude(id, url) {
+  fetch(url)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById(id).innerHTML = html;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadInclude("site-header", "/includes/header.html");
+  loadInclude("site-footer", "/includes/footer.html");
+});
