55aa57fc5d29d734a5f3e9178169299df4df8e2c
venkatpalika-oss <venkat.palika@gmail.com>
2026-03-09 00:25:56 +0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -434,3 +434,13 @@ progressBar.style.width="0%";
   initAuthUI();
 
 });
+/* ==========================================
+   Cursor Glow Effect
+========================================== */
+
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
+});
